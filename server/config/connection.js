c6d6afe3e4f6f6174647edc4ddeebaeabf56ed8b
sangeethaNR <sangeethajadhav123@gmail.com>
2021-11-09 21:34:02 -0800

    latest mongoose version added

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,9 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/daycaredb', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/daycaredb', 
+  err => {
+    if(err) throw err;
+    console.log('connected to MongoDB')
 });
 
 module.exports = mongoose.connection;
