6ad261d2c9e68298091b2c6fc1345f1bba065af5
sangeethaNR <sangeethajadhav123@gmail.com>
2021-11-09 20:52:00 -0800

    addded profile schema

---
+++ server/models/Profile.js
@@ -0,0 +1,108 @@
+const { Schema } = require('mongoose');
+
+const profileSchema = new Schema({
+
+    name :{
+        type :String,
+        required : true,
+    },
+    middleName:{
+        type :String,
+    },
+    lastName:{
+        type: String,
+    },
+    dateOfBirth: {
+        type: Date,
+        required: true,
+        trim: true,
+    },
+    joinedOn : {
+        type: Date,
+        required: true,
+        trim: true,
+    },
+    allergy:[
+        {
+            type: String,
+          },
+    ],
+parents :[
+{
+    parentName:{
+        type :String,
+        required : true,
+    },
+    phoneNo:{
+        type:Number,
+        required:true,
+    },
+    address:{
+        type:String,
+        required:true
+    },
+    relation:{
+        type:String,
+        required:true
+    },
+}],
+emergencyContact : [ 
+    {
+        contactPerson :{
+            type:String,
+            required:true
+        },
+        relationship : {
+            type:String,
+            required:true
+        },
+        phoneNo : {
+            type:Number,
+            required:true
+        },
+    }
+],
+physician : [ 
+    {
+        physicianName : {
+            type:String,
+            required:true
+        },
+        medicalRecordNo : {
+            type:Number,
+            required:true
+        },
+        address :{
+            type:String,
+            required:true
+        },
+        phoneNo : {
+            type:String,
+            required:true
+        },
+    }
+],
+isPottyTrained : true,
+medications : [ 
+    {
+        medName : {
+            type:String,
+            required:true
+        },
+        dosage: {
+            type:String,
+            required:true
+        },
+        profilePic: {
+            type:String,
+            required:true
+        },
+    }
+]
+
+})
+
+
+const Professor = model('Profile', professorSchema);
+
+module.exports = profileSchema;
