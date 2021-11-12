7cecb0defc45630d374c1de1c3c37dbd219e31ee
sangeethaNR <sangeethajadhav123@gmail.com>
2021-11-11 16:00:52 -0800

    Activity model added

--- server/models/index.js
+++ server/models/index.js
@@ -3,5 +3,6 @@ const Profile = require('./Profile');
 const Photo = require('./Photo');
 const IncidentReport = require('./IncidentReport');
 const ClassRoom = require('./ClassRooms');
+const Activity= require('./Activity');
 
-module.exports = { User,Profile,Photo,IncidentReport,ClassRoom };
+module.exports = { User,Profile,Photo,IncidentReport,ClassRoom,Activity };
