2b4cea538cd6a3e6fca7a96c19707f57ef5b9ca6
MSaideman <82477037+MSaideman@users.noreply.github.com>
2021-11-13 08:54:31 -0700

    added dashboard.js link to activities

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 function Dashboard() {
-    return (
-      <div>
-        {}
-      </div>
-    );
-  }
+  return (
+    <Link as={Link} to="/activities">
+      Activities
+    </Link>
+  );
+}
 
 export default Dashboard;
