9f3806015b92d92d63385bd8cd622ea1d329e1a2
sangeethaNR <sangeethajadhav123@gmail.com>
2021-11-14 06:19:02 -0800

    added userseeds

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,9 +1,13 @@
 const db = require('../config/connection');
-const { Profile } = require('../models');
+const { User,Profile } = require('../models');
+const userSeeds = require('./userSeeds.json');
 const profileSeeds = require('./profileSeeds.json');
 
 db.once('open', async () => {
   await Profile.deleteMany({});
+  await User.deleteMany({});
+
+  await User.create(userSeeds);
   await Profile.create(profileSeeds);
 
   console.log('all done!');
