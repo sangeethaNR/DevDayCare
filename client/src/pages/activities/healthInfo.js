b173e8f7d984a588170201ad378b6a430961911a
MSaideman <82477037+MSaideman@users.noreply.github.com>
2021-11-15 21:06:15 -0600

    added physician to health info form

--- client/src/pages/activities/healthInfo.js
+++ client/src/pages/activities/healthInfo.js
@@ -4,9 +4,15 @@ function HealthInfo() {
   return (
     <form>
       <div class="form-box">
+        <label for="medications">Medication Info</label>
         <input placeholder="Medication" maxlength="28" type="text" />
         <input placeholder="Dosage" maxlength="28" type="text" />
         <button type="submit">Submit</button>
+        <label for="physician">Physician Info:</label>
+          <input placeholder="name" maxlength="20" type="text" />
+          <input placeholder="medical record number" maxlength="20" type="text" />
+          <textarea placeholder="address" maxlength="20" type="text" />
+          <input placeholder="phone number" maxlength="20" type="number" />
       </div>
     </form>
   );
