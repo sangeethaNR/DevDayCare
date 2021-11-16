a3c83d9b9e8b4b0c92a7d634c08c4458bc40c6a2
sangeethaNR <sangeethajadhav123@gmail.com>
2021-11-16 11:25:53 -0800

    added modal for photo upload

--- client/src/pages/activities/addPhoto.js
+++ client/src/pages/activities/addPhoto.js
@@ -5,7 +5,45 @@ import { set } from "mongoose";
 import { useParams } from "react-router-dom";
 import {useQuery,useMutation} from "@apollo/client"
 import {ADD_PHOTO} from '../../utils/mutations'
+import Alert from '@material-ui/lab/Alert';
+import AlertTitle from '@material-ui/lab/AlertTitle';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import Modal from '@material-ui/core/Modal';
 
+
+//TO Do : set popup when adding photo
+function rand() {
+  return Math.round(Math.random() * 20) - 10;
+}
+
+function getModalStyle() {
+  const top = 50 + rand();
+  const left = 50 + rand();
+  return {
+      top: `${top}%`,
+      left: `${left}%`,
+      transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+const useStyles = makeStyles(theme => ({
+  modal: {
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+  },
+  paper: {
+      position: 'absolute',
+      width: 450,
+      backgroundColor: theme.palette.background.paper,
+      boxShadow: theme.shadows[5],
+      padding: theme.spacing(2, 4, 3),
+  },
+}));
+
+
+ 
 
 // TO DO:add logic to upload photo
 
@@ -14,13 +52,25 @@ function AddPhoto() {
   const [addPhotoMutation] = useMutation(ADD_PHOTO)
 
 const[imageSelected,setImageSelected] = useState("")
+const classes = useStyles();
+  const [modalStyle] = React.useState(getModalStyle);
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => {
+      setOpen(true);
+  };
+
+  const handleClose = () => {
+      setOpen(false);
+  };
 const [saveImage,setSaveImage] = useState({student_id:itemId,
 imageUrl:''});
-const uploadImage=() =>{
+
+const uploadImage= async () =>{
   const formData = new FormData();
   formData.append("file",imageSelected);
   formData.append("upload_preset","vpdvr4v6");
-  Axios.post("https://api.cloudinary.com/v1_1/dhcq7qcuc/image/upload",
+   await Axios.post("https://api.cloudinary.com/v1_1/dhcq7qcuc/image/upload",
   formData)
   .then((response)=> {
     console.log(response.data.url)
@@ -36,14 +86,18 @@ const  saveImagetoDB = async() =>{
   const {data} = await addPhotoMutation({variables:{...saveImage}});
   console.log(data);
   if (!data) {
-    throw new Error("something went wrong!");
-  }
 
+    throw new Error("something went wrong!");
+   
+  }
+handleOpen()
  
 } catch (err) {
 console.error(err);
 
+
 }
+
 }
 
     return (
@@ -51,10 +105,27 @@ console.error(err);
       <div class="form-box">
          
       <label for="desc">Photo:</label>
-     <input type="file" onChange={(event) =>{setImageSelected(event.target.files[0])}}></input>
+     <input name ="fileInput" type="file" onChange={(event) =>{setImageSelected(event.target.files[0])}}></input>
       <button onClick={uploadImage}>Upload Image</button>
       {/* <Image cloudName= "dhcq7qcuc" publicId=""/> */}
+    
+      <Modal
+                aria-labelledby="simple-modal-title"
+                aria-describedby="simple-modal-description"
+                open={open}
+                onClose={handleClose}
+            >
+                <div style={modalStyle} className={classes.paper}>
+                    <h2>Success</h2>
+                    <p>
+                        Photo added successfully
+                    </p>
+                </div>
+            </Modal>
+     
+
       </div>
+      
   
 
     );
