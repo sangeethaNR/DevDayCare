997e804f209efa58fc393448c508e9eea011b5b1
Michael Medrano <mgmedrano@outlook.com>
2021-11-17 12:13:30 -0800

    Updated Navigator for Teacher Dash

--- client/src/dashboard/Header.js
+++ client/src/dashboard/Header.js
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import PropTypes from 'prop-types';
 import AppBar from '@mui/material/AppBar';
-import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import Grid from '@mui/material/Grid';
 import HelpIcon from '@mui/icons-material/Help';
@@ -14,6 +13,7 @@ import Tabs from '@mui/material/Tabs';
 import Toolbar from '@mui/material/Toolbar';
 import Tooltip from '@mui/material/Tooltip';
 import Typography from '@mui/material/Typography';
+import NavBar from '../components/NavBar';
 
 const lightColor = 'rgba(255, 255, 255, 0.7)';
 
@@ -22,7 +22,7 @@ function Header(props) {
 
   return (
     <React.Fragment>
-      <AppBar color="primary" position="sticky" elevation={0}>
+      {/* <AppBar color="primary" position="sticky" elevation={0}>
         <Toolbar>
           <Grid container spacing={1} alignItems="center">
             <Grid sx={{ display: { sm: 'none', xs: 'block' } }} item>
@@ -39,12 +39,11 @@ function Header(props) {
            
             <Grid item>
               <IconButton color="inherit" sx={{ p: 0.5 }}>
-                <Avatar src="/static/images/avatar/1.jpg" alt="My Avatar" />
               </IconButton>
             </Grid>
           </Grid>
         </Toolbar>
-      </AppBar>
+      </AppBar> */}
       <AppBar
         component="div"
         color="primary"
@@ -52,14 +51,12 @@ function Header(props) {
         elevation={0}
         sx={{ zIndex: 0 }}
       >
-        <Toolbar>
+        <NavBar />
+        {/* <Toolbar>
           <Grid container alignItems="center" spacing={1}>
             <Grid item xs>
-              <Typography color="inherit" variant="h5" component="h1">
-                Authentication
-              </Typography>
-            </Grid>
-            <Grid item>
+            </Grid> */}
+            {/* <Grid item>
               <Button
                 sx={{ borderColor: lightColor }}
                 variant="outlined"
@@ -68,7 +65,7 @@ function Header(props) {
               >
                 Log out
               </Button>
-            </Grid>
+            </Grid> */}
             {/* <Grid item>
               <Tooltip title="Help">
                 <IconButton color="inherit">
@@ -76,16 +73,9 @@ function Header(props) {
                 </IconButton>
               </Tooltip>
             </Grid> */}
-          </Grid>
-        </Toolbar>
-      </AppBar>
-      <AppBar component="div" position="static" elevation={0} sx={{ zIndex: 0 }}>
-        <Tabs value={0} textColor="inherit">
-          <Tab label="Users" />
-          <Tab label="Sign-in method" />
-          <Tab label="Templates" />
-          <Tab label="Usage" />
-        </Tabs>
+          {/* </Grid>
+        </Toolbar> */}
+        <NavBar />
       </AppBar>
     </React.Fragment>
   );
