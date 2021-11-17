b9142b4ad4ae5ecaf1982eb95d3ea8c821012706
Michael Medrano <mgmedrano@outlook.com>
2021-11-17 13:24:40 -0800

    Updates to TeacherDash and pages for activities

--- client/src/pages/activities/incidents.js
+++ client/src/pages/activities/incidents.js
@@ -1,17 +1,39 @@
 import * as React from 'react';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import Divider from '@mui/material/Divider';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import Avatar from '@mui/material/Avatar';
+import Typography from '@mui/material/Typography';
 
 function Incidents() {
 
     return (
-      <form>
-      <div class="form-box">
+        <form>
+      <div class="form-box" style={{border: "dotted pink 2vw"}} variant="outlined" gutterBottom>
       <label for="desc">Incident:</label>
       <input placeholder="write report here" maxlength="28" type="text" />
       <button type="submit">Submit</button>
-      </div>
+      <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+      <ListItem alignItems="flex-start">
+      </ListItem>
+      <Divider variant="inset" component="li" />
+      <ListItem alignItems="flex-start">
+        <ListItemText
+          primary={
+            <React.Fragment>
+              {"PREVIOUS INCIDENTS"}
+            </React.Fragment>
+          }
+        />
+        <Divider variant="inset" component="li" />
+      </ListItem>
+    </List>
+    </div>
     </form>
+  );
+}
 
-    );
-  }
 
 export default Incidents;
--- client/src/pages/activities/notes.js
+++ client/src/pages/activities/notes.js
@@ -1,14 +1,33 @@
 import * as React from "react";
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import Divider from '@mui/material/Divider';
+import ListItemText from '@mui/material/ListItemText';
 
 function Notes() {
   return (
     <form>
-      <div class="form-box">
-      <label for="noteDesc">Notes:</label>
-      <input placeholder="write notes here" maxlength="28" type="text" />
-      <button type="submit">Submit</button>
-      </div>
-    </form>
+    <div class="form-box" style={{border: "dotted pink 2vw"}} variant="outlined" gutterBottom>
+    <label for="desc">Child Note:</label>
+    <input placeholder="Your Note" maxlength="28" type="text" />
+    <button type="submit">Submit</button>
+    <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+    <ListItem alignItems="flex-start">
+    </ListItem>
+    <Divider variant="inset" component="li" />
+    <ListItem alignItems="flex-start">
+      <ListItemText
+        primary={
+          <React.Fragment>
+            {"PREVIOUS NOTES"}
+          </React.Fragment>
+        }
+      />
+      <Divider variant="inset" component="li" />
+    </ListItem>
+  </List>
+  </div>
+  </form>
   );
 }
 
