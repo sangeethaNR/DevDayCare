99a0186cfe8a5a64f7904fd66eb04b48909b94d9
MSaideman <82477037+MSaideman@users.noreply.github.com>
2021-11-18 20:48:10 -0600

    removed buttons teacher dashboard

--- client/src/pages/teacherDashboardPage.js
+++ client/src/pages/teacherDashboardPage.js
@@ -4,6 +4,7 @@ import { GET_TEACHER_PROFILE } from "../utils/queries";
 import { Link } from "react-router-dom";
 import Auth from "../utils/auth";
 import TeacherDashboard from "../components/TeacherDashboard";
+import { Navbar, Nav, Container, Modal, Tab } from "react-bootstrap";
 
 function TeacherDashboardPage() {
   const user_id = Auth.getProfile()?.data._id;
@@ -15,48 +16,23 @@ function TeacherDashboardPage() {
   const userDetails = data?.getTeacherProfile || {};
   return (
     <React.Fragment>
+         <Navbar variant="light" expand="lg" className="brand">
+          <Container
+            fluid
+            style={{ fontFamily: "Comic Sans Ms", fontSize: "1.3rem" }}
+          >
+            {/* TO DO:link to home page */}
+            <Navbar.Brand>
+              <h1>Blossom Babies</h1>
+            </Navbar.Brand>
+            </Container>
+        </Navbar>
       <div style={{ border: "dotted pink 2vw", height: "100vh", padding:'2rem' }}>
         {userDetails.is_active ? (
           <TeacherDashboard user_id={user_id}/>
         ) : (
           <h1>Your profile is inactive waiting for activation</h1>
         )}
-        <div style={{display:"flex", flexDirection:"row", justifyContent:"space-evenly"}}>
-        <Link
-          as={Link}
-          to="/addStudent"
-          style={{
-            textDecoration: "none",
-            fontFamily: "Comic Sans Ms",
-            fontSize: "1.3rem",
-            color: "black",
-            border: "solid black .1vw",
-            padding: ".25rem",
-            borderRadius: "1rem",
-            backgroundColor: "lightblue",
-            margin: "5rem",
-          }}
-        >
-          Add Student
-        </Link>
-        <Link
-          as={Link}
-          to="/addClassroom"
-          style={{
-            textDecoration: "none",
-            fontFamily: "Comic Sans Ms",
-            fontSize: "1.3rem",
-            color: "black",
-            border: "solid black .1vw",
-            padding: ".25rem",
-            borderRadius: "1rem",
-            backgroundColor: "lightblue",
-            margin: "5rem",
-          }}
-        >
-          Add Classroom
-        </Link>
-        </div>
       </div>
     </React.Fragment>
   );
